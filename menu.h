d312df05a907039419fdcf0a8b24213cc7a76c31
pedroBasto <pedrobasto18@gmail.com>
2020-12-09 16:26:56 +0000

    Ficheiros base
    Ficheiros base do projeto

---
+++ menu.h
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   menu.h
+ * Author: Gabriel
+ *
+ * Created on 9 de dezembro de 2020, 15:19
+ */
+
+#ifndef MENU_H
+#define MENU_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void menuPrincipal();
+
+void menuFuncionarios();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MENU_H */
+
