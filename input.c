a63e1a9ab88ddc94904c1f847afdb683fd4559a3
MariaAlves12 <75744432+MariaAlves12@users.noreply.github.com>
2021-01-03 15:35:11 +0000

    Update input.c

--- input.c
+++ input.c
@@ -10,80 +10,83 @@
 
 #define VALOR_INVALIDO "O valor inserido é inválido."
 
-/*
-void cleanInputBuffer() { //ver isto!!
+void clean_input_buffer() { //ver isto!!
     char ch;
     while ((ch = getchar()) != '\n' && ch != EOF);
 }
-*/
 
-int obterInt(int minValor, int maxValor, char *msg) {
+
+
+int obter_int(int minValor, int maxValor, char *msg) {
     int valor;
     printf(msg);
     while (scanf("%d", &valor) != 1 || valor < minValor || valor > maxValor) {
         puts(VALOR_INVALIDO);
-/*
-        cleanInputBuffer();
-*/
+
+        clean_input_buffer();
+
         printf(msg);
     }
-/*
-    cleanInputBuffer();
-*/
+
+    clean_input_buffer();
+
     return valor;
 }
 
-float obterFloat(float minValor, float maxValor, char *msg) {
+float obter_float(float minValor, float maxValor, char *msg) {
     float valor;
     printf(msg);
     while (scanf("%f", &valor) != 1 || valor < minValor || valor > maxValor) {
         puts(VALOR_INVALIDO);
-/*
-        cleanInputBuffer();
-*/
+
+        clean_input_buffer();
+
         printf(msg);
     }
-/*
-    cleanInputBuffer();
-*/
+
+    clean_input_buffer();
+
     return valor;
 }
 
-double obterDouble(double minValor, double maxValor, char *msg) {
+double obter_double(double minValor, double maxValor, char *msg) {
     double valor;
     printf(msg);
     while (scanf("%lf", &valor) != 1 || valor < minValor || valor > maxValor) {
         puts(VALOR_INVALIDO);
-/*
-        cleanInputBuffer();
-*/
+
+        clean_input_buffer();
+
         printf(msg);
     }
-/*
-    cleanInputBuffer();
-*/
+
+    clean_input_buffer();
+
     return valor;
 }
 
-void lerString(char *string, unsigned int tamanho, char *msg) {
+void ler_string(char *string, unsigned int tamanho, char *msg) {
     printf(msg);
     if (fgets(string, tamanho, stdin) != NULL) {
         unsigned int len = strlen(string) - 1;
         if (string[len] == '\n') {
             string[len] = '\0';
         } else {
-/*
-            cleanInputBuffer();
-*/
+
+            clean_input_buffer();
+
         }
     }
 }
-char obterChar(char *msg) {
+
+
+
+
+char obter_char(char *msg) {
     char valor;
     printf(msg);
     valor = getchar();
-/*
-    cleanInputBuffer();
-*/
+    clean_input_buffer();
     return valor;
 }
+
